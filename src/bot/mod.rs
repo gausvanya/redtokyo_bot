8c811e822ca8f3fbb79b7f07769335ca8197a229
gausi <gaus.vanya_2006@mail.ru>
2026-08-09 07:44:07 +0300

    переход с webhook на long polling

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -12,37 +12,10 @@ mod utils;
 use crate::bot::handlers::register_routers;
 use crate::bot::task::verbal_warns_clear_task;
 use crate::config::Config;
-use axum::Router as AxumRouter;
 use sea_orm::DatabaseConnection;
-use std::fmt::Display;
 use telers::enums::UpdateType;
-use telers::event::simple;
-use telers::methods::SetWebhook;
-use telers::webhooks::axum::{UpdatesHandler, get_updates_router};
 use telers::{Bot, Dispatcher, Router};
-use tokio::net::TcpListener;
 use tokio::sync::broadcast::{Receiver, Sender, channel};
-
-async fn set_webhook(
-    bot: Bot,
-    webhook_url: impl Display,
-    webhook_path: impl Display,
-    secret_token: Option<impl Into<Box<str>>>,
-) -> simple::HandlerResult {
-    bot.send(
-        SetWebhook::new(format!("{webhook_url}{webhook_path}"))
-            .allowed_updates(vec![
-                UpdateType::Message,
-                UpdateType::CallbackQuery,
-                UpdateType::ChatMember,
-                UpdateType::MyChatMember,
-                UpdateType::ChatJoinRequest,
-            ])
-            .secret_token_option(secret_token),
-    )
-    .await?;
-    Ok(())
-}
 
 fn load_middleware(router: Router) -> Router {
     router.on_update(|observer| {
@@ -52,48 +25,31 @@ fn load_middleware(router: Router) -> Router {
             .register_outer_middleware(middlewares::antispam::AntispamMiddleware)
     })
 }
+
 pub async fn start(cfg: &Config, db: DatabaseConnection) -> anyhow::Result<()> {
     let bot = Bot::new(cfg.bot_token.to_string());
     let mut main_router = register_routers();
 
     main_router = load_middleware(main_router);
 
-    // let _ = bot
-    //     .send(DeleteWebhook::new().drop_pending_updates(true))
-    //     .await;
-
-    let webhook_url = cfg.webhook_url.clone();
-    let webhook_path = cfg.webhook_path.clone();
-    let secret_token = cfg.secret_token.clone();
-
-    main_router = main_router.on_startup(|observer| {
-        observer.register(simple::Handler::new(
-            set_webhook,
-            (
-                bot.clone(),
-                webhook_url,
-                webhook_path.clone(),
-                Some(secret_token.clone()),
-            ),
-        ))
-    });
+    let allowed_updates = [
+        UpdateType::Message,
+        UpdateType::CallbackQuery,
+        UpdateType::ChatMember,
+        UpdateType::MyChatMember,
+        UpdateType::ChatJoinRequest,
+    ];
 
     let dispatcher = Dispatcher::builder()
         .main_router(main_router.configure_default())
+        .allowed_updates(allowed_updates)
         .bot(bot.clone())
         .extension(db.clone())
         .build();
 
-    let app = AxumRouter::new().route(
-        webhook_path.as_ref(),
-        get_updates_router(UpdatesHandler::new(bot, dispatcher.clone()).secret_token(secret_token)),
-    );
-
     let (shutdown_tx, _) = channel(1);
 
-    let cfg_clone = cfg.clone();
     let _ = tokio::join!(
-        tokio::spawn(run_server(app, shutdown_tx.subscribe(), cfg_clone)),
         tokio::spawn(run_dispatcher(dispatcher, shutdown_tx.subscribe())),
         tokio::spawn(handle_shutdown(shutdown_tx)),
         tokio::spawn(verbal_warns_clear_task(db))
@@ -102,25 +58,9 @@ pub async fn start(cfg: &Config, db: DatabaseConnection) -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn run_server(app: AxumRouter, mut shutdown_rx: Receiver<()>, cfg: Config) {
-    let server_host = cfg.server_host.clone();
-    let server_port = cfg.server_port;
-
-    let listener = TcpListener::bind(format!("{server_host}:{server_port}"))
-        .await
-        .unwrap();
-
-    axum::serve(listener, app)
-        .with_graceful_shutdown(async move {
-            let _ = shutdown_rx.recv().await;
-        })
-        .await
-        .unwrap();
-}
-
 async fn run_dispatcher(dispatcher: Dispatcher, mut shutdown_rx: Receiver<()>) {
     dispatcher
-        .run_no_polling()
+        .run_polling()
         .with_graceful_shutdown(async move {
             let _ = shutdown_rx.recv().await;
         })
